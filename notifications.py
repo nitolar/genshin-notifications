eba37208394354ec4516e4b3257f18cf8ac6f50d
nitolar <73779998+nitolar@users.noreply.github.com>
2023-03-28 23:38:08 +0200

    Added daily check-in automatic collector and much more Check changelog section in readme.md for more info

--- notifications.py
+++ notifications.py
@@ -1,8 +1,8 @@
-import dotenv, os, pyttsx3
+import dotenv, os, pyttsx3, pytz, datetime
 import genshinstats as gs
 from win10toast import ToastNotifier
 from multiprocessing import Process
-from time import sleep
+from time import sleep, localtime, strftime
 
 dotenv.load_dotenv(dotenv_path="settings.env")
 toaster = ToastNotifier()
@@ -27,17 +27,35 @@ def resin():
             resin_milestones = os.getenv('resin_milestones').split(', ')
             if str(notes['resin']) in resin_milestones:
                 if resin_notification_send == False:
-                    print("One of your resin milestone was reached")
+                    print(f"{strftime('%H:%M:%S', localtime())} | One of your resin milestone was reached")
                     if os.getenv('tts') == 'True':
                         engine.say("One of your resin milestone was reached")
                         engine.runAndWait()
                     resin_last_count = notes['resin']
                     toaster.show_toast("One of your resin milestone was reached", f"You currently have {notes['resin']} resin out of {notes['max_resin']}", "ico/Resin.ico", 60)
                     resin_notification_send = True
+            else:
+                if resin_notification_send == False:
+                    if notes['resin'] == notes['max_resin']:
+                        print(f"{strftime('%H:%M:%S', localtime())} | Your resin is FULL")
+                        if os.getenv('tts') == 'True':
+                            engine.say("Your resin is FULL")
+                            engine.runAndWait()
+                        resin_last_count = notes['resin']
+                        toaster.show_toast("Your resin is FULL", f"You currently have {notes['resin']} resin out of {notes['max_resin']}", "ico/Resin.ico", 10)
+                        resin_notification_send = True
+                    elif notes['resin'] >= notes['max_resin']:
+                        print(f"{strftime('%H:%M:%S', localtime())} | Your resin isn't just FULL")
+                        if os.getenv('tts') == 'True':
+                            engine.say("Your resin isn't just FULL")
+                            engine.runAndWait()
+                        resin_last_count = notes['resin']
+                        toaster.show_toast("Your resin isn't just FULL", f"You currently have {notes['resin']} resin out of {notes['max_resin']}", "ico/Resin.ico", 60)
+                        resin_notification_send = True    
         else: 
             if resin_notification_send == False:
                 if notes['resin'] == notes['max_resin']:
-                    print("Your resin is FULL")
+                    print(f"{strftime('%H:%M:%S', localtime())} | Your resin is FULL")
                     if os.getenv('tts') == 'True':
                         engine.say("Your resin is FULL")
                         engine.runAndWait()
@@ -45,7 +63,7 @@ def resin():
                     toaster.show_toast("Your resin is FULL", f"You currently have {notes['resin']} resin out of {notes['max_resin']}", "ico/Resin.ico", 10)
                     resin_notification_send = True
                 elif notes['resin'] >= notes['max_resin']:
-                    print("Your resin isn't just FULL")
+                    print(f"{strftime('%H:%M:%S', localtime())} | Your resin isn't just FULL")
                     if os.getenv('tts') == 'True':
                         engine.say("Your resin isn't just FULL")
                         engine.runAndWait()
@@ -69,22 +87,32 @@ def realm():
             realm_milestones = os.getenv('realm_milestones').split(', ')
             if str(notes['realm_currency']) in realm_milestones:
                 if realm_notification_send == False:
-                    print("One of your realm currency milestone was reached")
+                    print(f"{strftime('%H:%M:%S', localtime())} | One of your realm currency milestone was reached")
                     if os.getenv('tts') == 'True':
                         engine.say("One of your realm currency milestone was reached")
                         engine.runAndWait()
                     realm_last_count = notes['realm_currency']
                     toaster.show_toast("One of your realm currency milestone was reached", f"You currently have {notes['realm_currency']} realm currency out of {notes['max_realm_currency']}", "ico/Realm.ico", 60)
                     realm_notification_send = True
+            else:
+                if realm_notification_send == False:
+                    if notes['realm_currency'] == notes['max_realm_currency']:
+                        print(f"{strftime('%H:%M:%S', localtime())} | Your realm currency is FULL")
+                        if os.getenv('tts') == 'True':
+                            engine.say("Your realm currency is FULL")
+                            engine.runAndWait()
+                        realm_last_count = notes['realm_currency']
+                        toaster.show_toast("Your realm currency is FULL", f"You currently have {notes['realm_currency']} realm currency out of {notes['max_realm_currency']}", "ico/Realm.ico", 60)
+                        realm_notification_send = True
         else: 
             if realm_notification_send == False:
                 if notes['realm_currency'] == notes['max_realm_currency']:
-                    print("Your realm currency is FULL")
+                    print(f"{strftime('%H:%M:%S', localtime())} | Your realm currency is FULL")
                     if os.getenv('tts') == 'True':
                         engine.say("Your realm currency is FULL")
                         engine.runAndWait()
                     realm_last_count = notes['realm_currency']
-                    toaster.show_toast("Your realm currency is FULL", f"You currently have {notes['realm_currency']} resin out of {notes['max_realm_currency']}", "ico/Realm.ico", 60)
+                    toaster.show_toast("Your realm currency is FULL", f"You currently have {notes['realm_currency']} realm currency out of {notes['max_realm_currency']}", "ico/Realm.ico", 60)
                     realm_notification_send = True
                 
         sleep(300)
@@ -100,7 +128,7 @@ def transformer():
         
         if notes['parametric_transformer_cooldown_ended'] == True:
             if parametric_notification_send == False:
-                print("Parametric Transformer cooldown has ended")
+                print(f"{strftime('%H:%M:%S', localtime())} | Parametric Transformer cooldown has ended")
                 if os.getenv('tts') == 'True':
                     engine.say("Parametric Transformer cooldown has ended")
                     engine.runAndWait()
@@ -121,24 +149,46 @@ def expeditions():
         
         elif expeditions_notification_send == False:
             if list(map(lambda x:x['status']=='Finished', notes['expeditions'])).count(True) == notes['max_expeditions']:
-                print("All your expeditions have completed")
+                print(f"{strftime('%H:%M:%S', localtime())} | All your expeditions have completed")
                 if os.getenv('tts') == 'True':
                     engine.say("All your expeditions have completed")
                     engine.runAndWait()
                 expeditions_last_count = list(map(lambda x:x['status']=='Finished', notes['expeditions'])).count(True)
-                toaster.show_toast("All your expeditions have completed", f"{expeditions_last_count} expeditions are completed out of {notes['max_expeditions']}", "ico/Expedition.ico", 60)
+                toaster.show_toast("All your expeditions have completed", f"{expeditions_last_count} expeditions have completed out of {notes['max_expeditions']}", "ico/Expedition.ico", 60)
                 expeditions_notification_send = True
             elif list(map(lambda x:x['status']=='Finished', notes['expeditions'])).count(True) > expeditions_last_count:
-                print("Some of your expeditions are completed")
+                print(f"{strftime('%H:%M:%S', localtime())} | Some of your expeditions have completed")
                 if os.getenv('tts') == 'True':
-                    engine.say("Some of your expeditions are completed")
+                    engine.say("Some of your expeditions have completed")
                     engine.runAndWait()
                 expeditions_last_count = list(map(lambda x:x['status']=='Finished', notes['expeditions'])).count(True)
-                toaster.show_toast("Some of your expeditions are completed", f"{expeditions_last_count} expeditions are completed out of {notes['max_expeditions']}", "ico/Expedition.ico", 60)
+                toaster.show_toast("Some of your expeditions have completed", f"{expeditions_last_count} expeditions have completed out of {notes['max_expeditions']}", "ico/Expedition.ico", 60)
                 expeditions_notification_send = True
                 
                 
         sleep(600)
+        
+def daily():
+    daily_last_day = -1
+    timezone = pytz.timezone('Etc/GMT-8')
+    while (True):
+        day = datetime.datetime.now(timezone).strftime('%d')
+        
+        if daily_last_day != day:
+            reward = gs.claim_daily_reward()
+            if reward is not None:
+                print(f"{strftime('%H:%M:%S', localtime())} | Claimed daily reward - {reward['cnt']}x {reward['name']}")
+                if os.getenv('tts') == 'True':
+                    engine.say("Collected your daily check-in reward")
+                    engine.runAndWait()
+                daily_last_day = day
+                if os.getenv('daily_not') == 'True':
+                    toaster.show_toast("Collected your daily check-in reward", f"Claimed daily reward - {reward['cnt']}x {reward['name']}", "ico/Daily.ico", 60)
+            else:
+                daily_last_day = day
+                
+                
+        sleep(900)
         
 if __name__ == '__main__':
     if (os.getenv('resin_not')) == 'True':
@@ -157,3 +207,8 @@ if __name__ == '__main__':
         p4 = Process(target=expeditions)
         p4.start()
         print('Expeditions turned on')
+    if (os.getenv('daily_check_in')) == 'True':
+        p5 = Process(target=daily)
+        p5.start()
+        print('Daily check-in turned on')
+    print("-----------------------------------")
--- readme.md
+++ readme.md
@@ -6,6 +6,8 @@
 
 Brings resin, realm currency, parametric transformer and expedition notifications to your windows PC!
 
+Also automatically collects your daily check-in!
+
 ## Preview
 
 [gsnotification.webm](https://user-images.githubusercontent.com/73779998/220456288-f21d8e94-0f3d-42f1-8458-4d93b65078d6.webm)
@@ -41,12 +43,34 @@ python notifications.py
 ```
 
 
+## Changelog
+
+### 28.03.2023
+
+**After this update you must reinstall requirements.txt! Or install pytz using: pip install pytz**
+- Changed some spelling
+- Added auto daily check-in
+- Fixed full resin and full realm currency notification not being send when milestones were turnd on
+- Added time when notification was sent in console
+- Added changelog to readme.md
+
+### 22.02.2023
+
+- Fixed resin and realm currency notification being send more than 1 time if it's full
+
+### 21.02.2023
+
+- First release
+
+
 ## Thanks to
 
 [thesadru](https://github.com/thesadru) for creating original [genshinstats](https://github.com/thesadru/genshinstats). [(My fork of genshinstats that is used in this project)](https://github.com/nitolar/genshinstats)
 
 
 ## Feedback
+
+Like what you see? Give a star if you don't mind.
 
 Found any bugs? Report them here: https://github.com/nitolar/genshin-notifications/issues
 
